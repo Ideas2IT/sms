9a2c4d300ba70178d8e2616e4e509c4184f0157e
karthicki2i <karthickg.ideas2it@gmail.com>
2011-01-28 14:38:32 +0530

    suggesting user with keywords functionality added

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,3 +1,14 @@
 class Action < ActiveRecord::Base
   has_many :inbound_sms
+  
+  class << self
+    
+    def get_keywords
+      action = Action.find(:all,:select=>"keyword")
+      keywords = action.collect{|action| action.keyword}.join(',').to_s
+      keywords
+    end
+    
+  end
+      
 end
